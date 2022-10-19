2c8158b8c860bbb52a5e3071163a3e54e99002e6
karl-no <karl.publicidade@gmail.com>
2022-10-19 17:22:38 -0300

    Refatoração ao apontar o banco_de_dados.tabela em alguns arquivos

--- desafio10.sql
+++ desafio10.sql
@@ -2,6 +2,6 @@
 -- ordenados por created_by em que o created_by é maior ou igual a 3.
 -- Ordene também os resultados pelo id de forma crescente, como critério de desempate para a ordenação.
 
-SELECT * FROM purchase_orders
+SELECT * FROM northwind.purchase_orders
 WHERE created_by >= 3
 ORDER BY created_by DESC, id ASC;
--- desafio15.sql
+++ desafio15.sql
@@ -1,4 +1,4 @@
 -- 15 - Mostre somente as horas (sem os minutos e os segundos) da coluna submitted_date de todos registros
 -- da tabela purchase_orders.
 -- No resultado, a hora extraída da coluna submitted_date deve ser chamada de submitted_hour.
-SELECT HOUR(submitted_date) AS submitted_hour FROM purchase_orders;
+SELECT HOUR(submitted_date) AS submitted_hour FROM northwind.purchase_orders;
