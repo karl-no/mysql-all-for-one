07fa0a3c924451d7299ecbc60885bf8f7f63a0d4
karl-no <karl.publicidade@gmail.com>
2022-10-19 17:58:04 -0300

    Requisito 23

---
+++ desafio23.sql
@@ -0,0 +1,6 @@
+-- 23 - Atualize os dados da coluna discount da tabela order_details para 30, onde o valor na coluna unit_price
+-- seja menor que 10.0000.
+-- Dica: Não é necessário utilizar o SAFE UPDATE em sua query.
+UPDATE order_details
+SET discount = 30
+WHERE unit_price < 10.0000;
