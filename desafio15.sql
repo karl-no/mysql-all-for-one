086b022d3536f224be599c4b53a5e0296fcc7331
karl-no <karl.publicidade@gmail.com>
2022-10-19 16:56:42 -0300

    Requisito 15

---
+++ desafio15.sql
@@ -0,0 +1,4 @@
+-- 15 - Mostre somente as horas (sem os minutos e os segundos) da coluna submitted_date de todos registros
+-- da tabela purchase_orders.
+-- No resultado, a hora extraída da coluna submitted_date deve ser chamada de submitted_hour.
+SELECT HOUR(submitted_date) AS submitted_hour FROM purchase_orders;
